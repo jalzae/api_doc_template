3c14c216e498666198cde8131dc2637532f073f1
JalzAe <jalzae@gmail.com>
2023-10-18 00:10:11 +0700

    update document

--- data.js
+++ data.js
@@ -6,44 +6,145 @@ var spec = {
   },
   "paths": {
     "/login": {
-      "post": {
-        "summary": "Create an article.",
-        "operationId": "createArticle",
+      "get": {
+        "summary": "Get Article.",
+        "operationId": "get and Article",
         "tags": [
           "App/Controller/Auth.php"
         ],
-        "requestBody": {
-          "content": {
-            "application/json": {
-              "username": "",
-              "password": ""
-            }
-          }
-        },
+
         "responses": {
           "200": {
-            "message": "Success",
+            "description": "{'status':true,'message':'berhasil'}",
+          },
+          "400": {
+            "description": "{'status':false,'message':'data error'}",
           },
         }
       },
     },
-    "/register": {
+    "/update/:id": {
       "post": {
         "summary": "Register",
         "operationId": "register",
         "tags": [
           "App/Controller/Auth.php"
         ],
+        "parameters": [
+          {
+            "$ref": "#/components/parameters/Id"
+          }
+        ],
+        "requestBody": {
+          "content": {
+            "application/json": {
+              "schema": {
+                "type": "object",
+                "properties": {
+                  "Article": {
+                    "type": "string",
+                    "description": "The content of the article."
+                  },
+                  "ArticleId": {
+                    "type": "integer",
+                    "format": "int32",
+                    "description": "The ID of the article."
+                  }
+                }
+              }
 
+            }
+          }
+        },
         "responses": {
-          "body": {
-            "description": "{username:'',password:''}",
+          "200": {
+            "description": "{'status':true,'message':'berhasil'}",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "object",
+                  "properties": {
+                    "Article": {
+                      "type": "string",
+                      "description": "The content of the article."
+                    },
+                    "ArticleId": {
+                      "type": "integer",
+                      "format": "int32",
+                      "description": "The ID of the article."
+                    }
+                  }
+                }
+              }
+            }
           },
+          "404": {
+            "description": "{'status':false,'message':'Data not found'}",
+            "content": {
+              "application/json": {
 
+                "schema": { "$ref": "#/components/schemas/NotFound" }
+              }
+            }
+          }
         }
       },
     },
-
   },
+  "components": {
+    "schemas": {
+      "Id": {
+        "description": "Resource ID",
+        "type": "integer/char",
+        "format": "int64/char36",
+        "readOnly": true,
+      },
+      "NotFound": {
+        "properties": {
+          "Status": {
+            "type": "boolean",
+            "description": "status"
+          },
+          "Message": {
+            "type": "string",
+            "description": "Tidak ditemukan data."
 
+          }
+        }
+      }
+    },
+    "parameters": {
+      "Id": {
+        "name": "id",
+        "in": "path",
+        "description": "Resource ID",
+        "required": true,
+        "schema": {
+          "$ref": "#/components/schemas/Id"
+        }
+      },
+    },
+    "responses": {
+      "NotFound": {
+        "description": "The resource is not found.",
+        "content": {
+          "application/json": {
+            "Error": {
+              "description": "<table>\n  <tr>\n    <th>Code</th>\n    <th>Description</th>\n  </tr>\n  <tr>\n    <td>illegal_input</td>\n    <td>The input is invalid.</td>\n  </tr>\n  <tr>\n    <td>not_found</td>\n    <td>The resource is not found.</td>\n  </tr>\n</table>\n",
+              "required": [
+                "code",
+                "message"
+              ],
+              "properties": {
+                "code": {
+                  "type": "string",
+                  "example": "illegal_input"
+                }
+              }
+            }
+          }
+        }
+      },
+    }
+  }
 }
